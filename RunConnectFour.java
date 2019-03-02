2446a8c73761405fc2541d5d65a0c2160c8c8e7d
JordynEMartin <48067854+JordynEMartin@users.noreply.github.com>
2019-03-02 13:30:03 -0600

    Add files via upload

---
+++ RunConnectFour.java
@@ -0,0 +1,23 @@
+// RunConnectFour.java
+// 2-19-19
+//
+// This will run the Connect Four game.
+
+import java.awt.*;
+import javax.swing.*;
+
+public class RunConnectFour
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Connect Four");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ConnectFourFrame game = new ConnectFourFrame();
+        frame.getContentPane().add(game);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
